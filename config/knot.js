67d06158e0b5af312df1480cb703a47b667d6915
Joedyson Bezerra <joedysonbezerra@gmail.com>
2019-05-30 15:48:05 -0300

    Configuração dos dados da cloud e gateway

---
+++ config/knot.js
@@ -0,0 +1,8 @@
+const KNoTCloud = require("knot-cloud");
+
+module.exports = new KNoTCloud(
+  "v1.knot.cloud",
+  80,
+  "e4d78fed-af7e-4249-945b-a01908640000",
+  "469d7e3b274fc1d71407d724f0f032b5bc4f64a5"
+);
