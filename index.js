70d7428be3369087d020b13b3bc38573d1023c29
Joedyson Bezerra <joedysonbezerra@gmail.com>
2019-06-01 23:59:12 -0300

    Adicionado cors e sentry

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-//const [device] = devices.filter(({ name }) => name === "joedyson");
-
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 const path = require("path");
 const dbConfig = require("./config/database.json");
+const sentryConfig = require("./config/sentry");
 const app = express();
+const cors = require("cors");
+
+Sentry.init(sentryConfig);
 
 mongoose.connect(dbConfig.url, dbConfig.flag);
 
 const models = path.resolve("src", "models");
 requireDir(models);
 
-app.use(express.json());
-app.use("/api", require("./src/routes"));
+app.use(cors());
 
-app.listen(3020, () => {
+app.use(express.json());
+app.use(Sentry.Handlers.requestHandler());
+app.use("/api", require("./src/routes"));
+app.use(Sentry.Handlers.errorHandler());
+
+app.listen(3080, () => {
   console.log("Servidor Iniciado");
 });
