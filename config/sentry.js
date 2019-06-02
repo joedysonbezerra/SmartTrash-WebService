659b0daaaacda8cdd84627bd71f12a68ac1b5072
Joedyson Bezerra <joedysonbezerra@gmail.com>
2019-06-02 00:14:12 -0300

    adicionado variaveis de ambiente e procfile

--- config/sentry.js
+++ config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: "https://b24a339e2a9c43dca4a43a7d4d9710d8@sentry.io/1472958"
+  dsn: process.env.SENTRY_DSN
 };
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const Sentry = require("@sentry/node");
 const path = require("path");
-const dbConfig = require("./config/database.json");
+const dbConfig = require("./config/database");
 const sentryConfig = require("./config/sentry");
-const app = express();
 const cors = require("cors");
+const app = express();
 
 Sentry.init(sentryConfig);
 
@@ -21,6 +22,6 @@ app.use(Sentry.Handlers.requestHandler());
 app.use("/api", require("./src/routes"));
 app.use(Sentry.Handlers.errorHandler());
 
-app.listen(3080, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor Iniciado");
 });
