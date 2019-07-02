abd8aacbca7d4e570d06f5ffa918eedb40b111fa
JohnGeovanne <geovanneoliveira3@gmail.com>
2019-07-02 11:37:28 -0300

    sempre alterar as configurações no Knot ao mudar a cloud

--- config/knot.js
+++ config/knot.js
@@ -1,8 +1,8 @@
 const KNoTCloud = require("knot-cloud");
 
 module.exports = new KNoTCloud(
-  "v1.knot.cloud",
-  80,
-  "e4d78fed-af7e-4249-945b-a01908640000",
-  "469d7e3b274fc1d71407d724f0f032b5bc4f64a5"
+  "10.0.0.101",
+  3000,
+  "22882cb4-1d0f-4bc3-9315-cea198e40000",
+  "2582d1213b92defe8a0172369b657faaec35a954"
 );
